199b7bf4bff34fbf3a35357992e2933ca50880ca
Elliott Hill <elliott.hill@gmail.com>
2024-04-03 15:16:49 +0100

    Add REPL

--- golox.go
+++ golox.go
@@ -1,51 +1,96 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"os"
+    "flag"
 )
 
 func main() {
 
-    if len(os.Args) < 2 {
-        fmt.Println("Error: please provide a file arg")
-        return
+    var file string
+    var debug bool
+
+    flag.StringVar(&file, "f", "", "Input File")
+    flag.BoolVar(&debug, "d", false, "Debug Mode")
+    flag.Parse()
+
+    if len(file) > 0 {
+         sourceCode, error := loadFromFile(file)
+
+        if error != nil {
+            fmt.Println(error);
+            return;
+        }
+
+        Run(sourceCode, &Interpreter{}, debug)
+
+    } else {
+
+        REPL(debug)
     }
 
-    sourceCode, error := loadFromFile(os.Args[1])
+}
 
-    if error != nil {
-        fmt.Println(error);
-        return;
-    }
+func Run(source string, interpreter *Interpreter, debug bool) {
 
-    scanner := Scanner{source: sourceCode}
+    scanner := Scanner{source: source}
     tokens := scanner.Scan()
 
+    if debug {
 
-    for _, element := range tokens{
-        fmt.Println(element)
+        fmt.Println("== Tokens ==")
+
+        for _, element := range tokens{
+            fmt.Println(element)
+        }
     }
 
     parser := Parser{tokens: tokens}
     statements := parser.Parse()
 
+    if debug {
+        fmt.Println("== Parse Tree ==")
+        for _, element := range statements {
 
-    fmt.Println("== Parsed Tree ==")
-    for _, element := range statements {
+            fmt.Println(element)
+        }
 
-        fmt.Println(element)
+        fmt.Println("== Interp ==")
     }
 
-    fmt.Println("== Interp ==")
-    interpreter := Interpreter{statements: statements}
-    interpreter.Interpret()
 
-    return str
+    interpreter.statements = statements
+    interpreter.Interpret()
 
 }
 
+
+func REPL(debug bool) {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("> ")
+    interp :=  Interpreter{}
+
+    for {
+        line, _, err := reader.ReadLine()
+
+        if err != nil {
+            fmt.Println(err)
+            os.Exit(64)
+        }
+
+        if (string(line) == "exit" || string(line) == "exit()") {
+            os.Exit(1)
+        }
+
+        Run(string(line), &interp, debug)
+        fmt.Print("> ")
+
+    }
+
+}
 
 
 func loadFromFile (filename string) (string, error) {
@@ -53,7 +98,7 @@ func loadFromFile (filename string) (string, error) {
     b, error := os.ReadFile(filename)
     if error != nil {
         fmt.Print(error)
-        return "", errors.New("Could not read filename");
+        return "", errors.New("Could not read filename " + filename);
     }
 
     contents := string(b)
